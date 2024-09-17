7bbad77a1c71ec41d6e4f4c8cd7d3f5e1b1709ea
unknown <angelika.lena.koeck@gmail.com>
2024-09-17 12:35:44 +0200

    project setup

--- PrimeSieve/src/EratosthenesPrimeSieve.java
+++ PrimeSieve/src/EratosthenesPrimeSieve.java
@@ -1,6 +1,41 @@
-public class EratosthenesPrimeSieve {
+public class EratosthenesPrimeSieve implements PrimeSieve{
 
-    public static void main(String[] args) {
+    public int ceiling = 500;
+    Boolean[] ceilingList = new Boolean[500];
+
+    public void checkIfNumberIsPrime()
+    {
+        int counter = 0;
+        int primeCounter = 2;
+
+        while(counter != ceiling)
+        {
+            if (counter % primeCounter)
+            {
+                ceilingList[counter] = true;
+            }else
+            {
+                primeCounter++;
+            }
+        }
+    }
+
+    @Override
+    public boolean isPrime(int p) {
+
+        int counter = 0;
+        for (Boolean b : ceilingList) {
+            if(counter == p)
+            {
+                return true;
+            }
+            counter++;
+        }
+        return false;
+    }
+
+    @Override
+    public void printPrimes() {
 
     }
 }
--- PrimeSieve/src/PrimeSieve.java
+++ PrimeSieve/src/PrimeSieve.java
@@ -2,7 +2,7 @@ public interface PrimeSieve {
 
     // U e b e r p r u ef t ob p ei n e Primzahl i s t ode r ni c h t .
     public boolean isPrime ( int p ) ;
-    
+
     // Gibt a l l e Primzahlen b i s zu r Obergrenze auf de r Konsole aus .
     public void printPrimes ( ) ;
 }
