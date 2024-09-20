e997e32be362f98b037c5fbb5c493ca0c132e0db
unknown <angelika.lena.koeck@gmail.com>
2024-09-20 20:20:22 +0200

    Programm works well

--- PrimeSieve/src/Main.java
+++ PrimeSieve/src/Main.java
@@ -7,7 +7,18 @@ public class Main {
 
         EratosthenesPrimeSieve eratosthenesPrimeSieve = new EratosthenesPrimeSieve();
         eratosthenesPrimeSieve.printPrimes();
-
-
+        while (true)
+        {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("\n Geben sie eine Zahl ein zwischen 1 und 500. Die wird geprüft ob sie eine Primzahl ist.");
+            int userNumber = Integer.parseInt(scanner.nextLine());
+            boolean isPrimeNumber = eratosthenesPrimeSieve.isPrime(userNumber);
+            if (isPrimeNumber == true)
+            {
+                System.out.println(userNumber + " ist eine Primzahl");
+                break;
+            }
+            System.out.println(userNumber + " ist keine Primzahl");
+        }
     }
 }
